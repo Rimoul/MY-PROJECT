6db05fbfde56e4c6b537e9455981a1b55894f232
MrBread <145653243+Rimoul@users.noreply.github.com>
2023-10-02 20:40:32 +0530

    Add files via upload

---
+++ Project 2/m_appScript.js
@@ -0,0 +1,28 @@
+const myformEl = document.getElementById("myform");                  
+const scoreEl = document.getElementById("score");                  
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+
+const n1 = Math.floor(Math.random()*10);
+const n2 = Math.floor(Math.random()*10);
+
+let rightans= n1*n2;
+questionEl.innerText= `What is ${n1} Multiply By ${n2}?`;
+let score= JSON.parse(localStorage.getItem('score'));
+
+if(score==null){score=0;}
+scoreEl.innerText= `Score: ${score}`;
+
+
+myformEl.addEventListener("submit",()=>{
+    const check=+answerEl.value;
+    if(rightans==check){score++;}
+    else{
+        if(score!=0){score--;}
+        else{score==0;}
+    }
+    inLocalStorage();
+})
+function inLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score));
+}
